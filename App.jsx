804269d275c873ba8f9a819abae014decacf7e58
Gabriel <gabrielsb1090@gmail.com>
2026-07-22 00:27:14 -0300

    feat: Subindo estado de nome / renderizando componentes

--- App.jsx
+++ App.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react"
 import Form from "./Components/Form/Form.jsx"
+import Perfil from "./Components/Perfil/Perfil.jsx"
 
 function App() {
+  const [nome, setNome] = useState("")
 
   return (
-    <Form />
-  )
+    <>
+      <Form 
+        nome={nome}
+        setNome={setNome}
+      />
 
+      <Perfil 
+        nome={nome}
+      />
+    </>
+  )
 }
 
 export default App
