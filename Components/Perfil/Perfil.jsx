4d32e8d9d3a214eeb471dffbf382b7458c44b12c
Gabriel <gabrielsb1090@gmail.com>
2026-07-22 04:37:11 -0300

    feat: Adicionando e exibindo albuns

--- Components/Perfil/Perfil.jsx
+++ Components/Perfil/Perfil.jsx
@@ -5,14 +5,14 @@ function Perfil({ nome }) {
 
     const [select, setSelect] = useState(null)
 
-    const artistas = [
-        {nome: "A$AP Rocky", album: "Testing"},
-        {nome: "Drake", album: "Views"},
-        {nome: "Kendrick Lamar", album: "Damn"},
-        {nome: "J. Cole", album: "Forest Hills" },
-        {nome: "Travis Scott", album: "Astroworld"},
-        {nome: "Kanye West", album: "Graduation"}
-    ]
+        const artistas = [
+            { nome: "A$AP Rocky", album: ["Live.Love.A$AP", "Long.Live.A$AP", "Testing"] },
+            { nome: "Drake", album: ["Views", "Scorpion", "Take Care"] },
+            { nome: "Kendrick Lamar", album: ["DAMN.", "good kid, m.A.A.d city", "To Pimp a Butterfly"] },
+            { nome: "J. Cole", album: ["Forest Hills Drive", "KOD", "The Off-Season"] },
+            { nome: "Travis Scott", album: ["Astroworld", "Rodeo", "Utopia"] },
+            { nome: "Kanye West", album: ["Graduation", "My Beautiful Dark Twisted Fantasy", "Yeezus"] }
+        ]
 
     function selectArtist(nome){
         setSelect(nome)
@@ -30,7 +30,9 @@ function Perfil({ nome }) {
                 ))}
                 
             {select && artistas.map((artista)=>(
-                select === artista.nome && <h3>{artista.album}</h3>
+                select === artista.nome && <h3>Albuns: <strong>{artista.album.map((album)=>(
+                    <h3>{album}</h3>
+                ))}</strong></h3>
                 ))}
             </div>
 
